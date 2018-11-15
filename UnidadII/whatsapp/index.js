149944fc8e5d5c5f9d6fdebf2df6decdf807eb92
Erick Marín <erick.marin.v@gmail.com>
2018-11-14 22:28:50 -0600

    Ejercicio Whatsapp

---
+++ UnidadII/whatsapp/index.js
@@ -0,0 +1,88 @@
+var express = require("express");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var app = express();
+var credenciales = {
+    host:"localhost",
+    user:"root",
+    password:"",
+    port:"3306",
+    database: "bd_whatsapp"
+};
+
+app.use(express.static("public"));
+
+app.get("/usuarios", function(req,res){
+    var conexion = mysql.createConnection(credenciales);
+    conexion.query(
+        "SELECT codigo_usuario, nombre_usuario, correo, contrasena, url_imagen_perfil FROM tbl_usuarios",
+        [],
+        function(error, data, fields){
+            console.log(error);
+            res.send(data);
+            res.end();
+            conexion.end();
+        }
+    );
+});
+
+app.get("/mensajes/:usuarioEmisor/:usuarioReceptor",function(req, res){
+    var conexion = mysql.createConnection(credenciales);
+    conexion.query(
+        `select  a.codigo_usuario_emisor,
+                a.codigo_usuario_receptor,
+                a.mensaje, a.hora_mensaje,
+                b.nombre_usuario as nombre_usuario_emisor,
+                c.nombre_usuario as nombre_usuario_receptor
+        from tbl_mensajes a
+        inner join tbl_usuarios b
+            on(a.codigo_usuario_emisor = b.codigo_usuario)
+        inner join tbl_usuarios c
+            on(a.codigo_usuario_receptor = c.codigo_usuario)
+        where (codigo_usuario_emisor = ? and codigo_usuario_receptor = ?)
+        or (codigo_usuario_emisor = ? and codigo_usuario_receptor = ?);`,
+        [
+            req.params.usuarioEmisor,
+            req.params.usuarioReceptor,
+            req.params.usuarioReceptor,
+            req.params.usuarioEmisor
+        ],
+        function(error, data, fields){
+            res.send(data);
+            res.end();
+        }
+    );
+    
+});
+
+
+//Esta ruta hace lo mismo que la anterior solo que se recibe los parametros via URLEncoded (GET)
+app.get("/mensajes",function(req,res){
+    var conexion = mysql.createConnection(credenciales);
+    conexion.query(
+        `select  a.codigo_usuario_emisor,
+                a.codigo_usuario_receptor,
+                a.mensaje, a.hora_mensaje,
+                b.nombre_usuario as nombre_usuario_emisor,
+                c.nombre_usuario as nombre_usuario_receptor
+        from tbl_mensajes a
+        inner join tbl_usuarios b
+            on(a.codigo_usuario_emisor = b.codigo_usuario)
+        inner join tbl_usuarios c
+            on(a.codigo_usuario_receptor = c.codigo_usuario)
+        where (codigo_usuario_emisor = ? and codigo_usuario_receptor = ?)
+        or (codigo_usuario_emisor = ? and codigo_usuario_receptor = ?);`,
+        [
+            req.query.usuarioEmisor,
+            req.query.usuarioReceptor,
+            req.query.usuarioReceptor,
+            req.query.usuarioEmisor
+        ],
+        function(error, data, fields){
+            res.send(data);
+            res.end();
+        }
+    );
+});
+
+app.listen(8111, function(){ console.log("Servidor iniciado");});
---
+++ UnidadII/whatsapp/public/js/controlador.js
@@ -0,0 +1,81 @@
+$("#slc-usuario").change(function(){
+	alert("USUARIO seleccionado: " + $("#slc-usuario").val());
+});
+
+function seleccionarContacto(codigoContacto, nombreContacto){
+	alert("Obtener conversación entre: " + codigoContacto + ",  y " + $("#slc-usuario").val());
+	console.log(`/mensajes/${$("#slc-usuario").val()}/${codigoContacto}`);
+	$.ajax({
+		url:`/mensajes/${$("#slc-usuario").val()}/${codigoContacto}`,
+		method:"GET",
+		dataType:"json",
+		success:function(res){
+			console.log(res);
+			$("#conversation").html("");
+			for (var i=0;i<res.length;i++){
+				var claseCss="receiver";
+				if (res[i].codigo_usuario_emisor == $("#slc-usuario").val())
+					claseCss="sender";
+				$("#conversation").append(
+					`<div class="row message-body">
+					<div class="col-sm-12 message-main-${claseCss}">
+					  <div class="${claseCss}">
+						<div class="message-text">
+						 ${res[i].mensaje}
+						</div>
+						<span class="message-time pull-right">
+						  ${res[i].hora_mensaje}
+						</span>
+					  </div>
+					</div>
+				  </div>`
+				);
+			}
+		},
+		error:function(error){
+			console.error(error);
+		}
+	});
+}
+
+$("#btn-enviar").click(function(){
+	alert("Enviar mensaje: " + $("#txta-mensaje").val());
+});
+
+$(document).ready(function(){
+	$.ajax({
+		url:"/usuarios",
+		method:"GET",
+		dataType:"json",
+		success:function(res){
+			console.log(res);
+			for (var i=0;i<res.length;i++){
+				$("#slc-usuario").append(`<option value="${res[i].codigo_usuario}">${res[i].nombre_usuario}</option>`);
+				$("#contactos").append(
+					`<div class="row sideBar-body" onclick="seleccionarContacto(${res[i].codigo_usuario},'${res[i].nombre_usuario}');">
+						<div class="col-sm-3 col-xs-3 sideBar-avatar">
+						<div class="avatar-icon">
+							<img src="${res[i].url_imagen_perfil}">
+						</div>
+						</div>
+						<div class="col-sm-9 col-xs-9 sideBar-main">
+						<div class="row">
+							<div class="col-sm-8 col-xs-8 sideBar-name">
+							<span class="name-meta">${res[i].nombre_usuario}
+							</span>
+							</div>
+							<div class="col-sm-4 col-xs-4 pull-right sideBar-time">
+							<span class="time-meta pull-right">18:18
+							</span>
+							</div>
+						</div>
+						</div>
+					</div>`
+				);
+			}
+		},
+		error:function(error){
+			console.error(error);
+		}
+	});
+});
